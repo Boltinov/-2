b751c04f673bc46ce97206af799f6d0f1c0ea040
Artur <Tamerlan_ka@mail.ru>
2021-05-10 14:58:16 +0300

    1

--- дз2/6.py
+++ дз2/6.py
@@ -19,21 +19,29 @@
  #   “ед”: [“шт.”]
 #}'''
 
-goods = []
-while input("хотите добавить новый товар? введите y/n: ") == 'y':
-    number = int(input("введите номер товара: "))
-    features = {}
-    while input("желаете добавить характеристики продукта? введите y/n: ") == 'y':
-        feature_key = input("введите название товара: ")
-        feature_value = input("введите цену товрара: ")
-        features[feature_key] = feature_value
-    goods.append(tuple([number, features]))
-print(goods)
-analitics = {}
-for good in goods:
-    for feature_key, feature_value in good[1].items():
-        if feature_key in analitics:
-            analitics[feature_key].append(feature_value)
-        else:
-         analitics[feature_key] = [feature_value]
-print(analitics)
+products, order = [], 1
+while True:
+    title = input("введите название товара : ")
+    price = input('введите цену товара :')
+    amount = input('введите количество :')
+    unit = input('введите еденицы измерения')
+    products.append((order, {'title': title,
+                             'price': price,
+                             'amount': amount,
+                             'unit': unit}))
+    order += 1
+    ask = input('завершено форматирование списка? (y/n)')
+    if ask == 'y':
+        break
+analitika = {'title': [],
+             'price': [],
+             'amount': [],
+             'unit': set()}
+for nomer, opisanie in products:
+    analitika['title'].append(opisanie['title'])
+    analitika['price'].append(opisanie['price'])
+    analitika['amount'].append(opisanie['amount'])
+    analitika['unit'].add(opisanie['unit'])
+
+print(products)
+print(analitika)
